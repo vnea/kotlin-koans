359cffaf624ca917df9b1983428b8546b9fb9021
Victor Nea <victor.nea@gmail.com>
2017-12-30 15:04:01 +0100

    _12_Extensions_On_Collections done

--- src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
+++ src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
@@ -2,6 +2,7 @@ package i_introduction._12_Extensions_On_Collections
 
 import util.TODO
 import util.doc12
+import java.util.*
 
 fun todoTask12(): Nothing = TODO(
     """
@@ -16,8 +17,15 @@ fun todoTask12(): Nothing = TODO(
     documentation = doc12()
 )
 
+fun List<Int>.sortedDescending() {
+    Collections.sort(this, { number1, number2 ->
+        number2 - number1
+    })
+}
+
 fun task12(): List<Int> {
-    todoTask12()
-    return arrayListOf(1, 5, 2)
+    val arrayList = arrayListOf(1, 5, 2)
+    arrayList.sortedDescending()
+    return arrayList
 }
 
