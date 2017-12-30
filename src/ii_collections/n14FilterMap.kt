9e9debcd2e0698972e32a5575af6e55c4ce3d0c8
Victor Nea <victor.nea@gmail.com>
2017-12-30 15:26:08 +0100

    N14FilterMapKtTest done

--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -10,12 +10,14 @@ fun example1(list: List<Int>) {
 
 fun Shop.getCitiesCustomersAreFrom(): Set<City> {
     // Return the set of cities the customers are from
-    todoCollectionTask()
+    val cities = HashSet<City>()
+    customers.mapTo(cities, Customer::city)
+    return cities
 }
 
 fun Shop.getCustomersFrom(city: City): List<Customer> {
     // Return a list of the customers who live in the given city
-    todoCollectionTask()
+    return customers.filter { customer -> customer.city == city }
 }
 
 
