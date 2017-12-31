350f70bfb647d056b491d14df1771e1e8d4196ab
Victor Nea <victor.nea@gmail.com>
2017-12-31 18:39:44 +0100

    N29OperatorsOverloadingKt done

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,5 +1,6 @@
 package iii_conventions
 
+// MyDate
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
     override fun compareTo(other: MyDate) = when {
         year != other.year -> year - other.year
@@ -8,14 +9,29 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparabl
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) = addTimeIntervals(
+    repeatedTimeInterval.timeInterval,
+    repeatedTimeInterval.nbTimes
+)
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+
+
+// TimeInterval
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
 
+operator fun TimeInterval.times(nbTimes: Int): RepeatedTimeInterval = RepeatedTimeInterval(this, nbTimes)
+
+// RepeatedTimeInterval
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val nbTimes: Int)
+
+// DateRange
 class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
     override fun iterator(): Iterator<MyDate> {
         return object : Iterator<MyDate> {
