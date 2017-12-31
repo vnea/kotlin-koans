19781048621da892b149c1617696779f1a81f483
Victor Nea <victor.nea@gmail.com>
2017-12-31 18:04:01 +0100

    N28ForLoopKt done

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,7 +1,5 @@
 package iii_conventions
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
     override fun compareTo(other: MyDate) = when {
         year != other.year -> year - other.year
@@ -18,8 +16,25 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current = start
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val nextDay = current
+                current = current.nextDay()
+
+                return nextDay
+            }
+        }
+    }
+
     operator fun contains(date: MyDate): Boolean {
-        return start <= date &&  date <= endInclusive
+        return date in start..endInclusive
     }
 }
