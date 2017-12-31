b33fa72135fd7ac1fff7871ee3195c680b1bb913
Victor Nea <victor.nea@gmail.com>
2017-12-31 03:05:43 +0100

    N26InRangeKt done

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,36 +1,14 @@
 package iii_conventions
 
-// BAD AND VERY UGLY VERSION (and maybe it doesn't work in every cases)
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    override fun compareTo(other: MyDate): Int {
-        if (year < other.year) {
-            return -1
-        }
+import com.sun.org.apache.xpath.internal.operations.Bool
 
-        if (year == other.year && month < other.month) {
-            return -1
-        }
-
-        if (year == other.year && month == other.month && dayOfMonth < other.dayOfMonth) {
-            return -1
-        }
-
-        if (dayOfMonth == other.dayOfMonth) {
-            return 0
-        }
-
-        return 1
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
     }
 }
-
-// BEST VERSION
-//data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
-//    override fun compareTo(other: MyDate) = when {
-//        year != other.year -> year - other.year
-//        month != other.month -> month - other.month
-//        else -> dayOfMonth - other.dayOfMonth
-//    }
-//}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -40,4 +18,8 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(date: MyDate): Boolean {
+        return start <= date &&  date <= endInclusive
+    }
+}
